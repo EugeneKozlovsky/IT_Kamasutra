4dd9c9119fc4d2065e38ab2954609f8e021cd195
Eugene Kozlovsky <ekozlovskii@gmail.com>
2023-05-07 22:20:49 +0300

    add solution  "Count by X" Codewars

--- main.js
+++ main.js
@@ -16,14 +16,22 @@ function bmi(weight, height) {
     } 
 }
 
-
 // Codewars, 'Function 1 - hello world'
+
 function greet() {
     return "hello world!";
     }
 
+/* Codewars, 'Count by X'
+Create a function with two arguments that will 
+return an array of the first n multiples of x.
+Assume both the given number and the number of times
+to count will be positive numbers greater than 0. */
 
-
-
-
-
+function countBy(x, n) {
+    let z = [];
+    for (let i = 1; i <= n; i++) {
+      z.push(i * x)
+    };
+    return z;
+  }
