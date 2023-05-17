8d4b71a013a9335e914f9a97139d92b43178818c
Eugene Kozlovsky <ekozlovskii@gmail.com>
2023-05-17 21:46:10 +0300

    add solution CodeWars "Traffic light"

--- codewars.js
+++ codewars.js
@@ -151,3 +151,23 @@ function createArray(number) {
 
     return newArray;
 }
+
+/* Thinkful - Logic Drills: Traffic light
+You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+For example, when the input is green, output should be yellow.
+*/
+
+function updateLight(current) {
+    switch (true) {
+        case (current == "green"):
+            return "yellow";
+            break;
+        case (current == "yellow"):
+            return "red";
+            break;
+        case (current == "red"):
+            return "green";
+            break;
+    }
+}
