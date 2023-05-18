c29d4f228341db96efaa96510ad00efd11873db3
Eugene Kozlovsky <ekozlovskii@gmail.com>
2023-05-18 15:18:32 +0300

    add solutions CodeWars "Expressions Matter"&"DNA"

--- codewars.js
+++ codewars.js
@@ -197,3 +197,33 @@ no matter if it was in the past or it is in the future.
 function twiceAsOld(dadYearsOld, sonYearsOld) {
     return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
+
+/* Expressions Matter
+Task
+Given three integers a ,b ,c, return the largest number obtained after inserting the following operators
+and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+*/
+function expressionMatter(a, b, c) {
+    return Math.max(
+        a + b + c,
+        a * b * c,
+        a * (b + c),
+        (a + b) * c,
+        a + b * c,
+        a * b + c,
+    );
+}
+
+/* DNA to RNA Conversion
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+Create a function which translates a given DNA string into RNA.
+For example:
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+*/
+
+function DNAtoRNA(dna) {
+    return dna.replace(/T/g, 'U');
+}
