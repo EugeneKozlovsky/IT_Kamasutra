eb65f124fb8cef20152d858ad0d72838c3bb1c28
Eugene Kozlovsky <ekozlovskii@gmail.com>
2023-05-19 20:26:32 +0300

    add solution CodeWars "Get the mean of an array"

--- codewars.js
+++ codewars.js
@@ -272,3 +272,13 @@ and false if they aren't.
 function lovefunc(flower1, flower2) {
     return (flower1 + flower2) % 2 == 1;
 }
+
+/* Get the mean of an array
+It's the academic year's end, fateful moment of your school report. The averages must be calculated.
+All the students come to you and entreat you to calculate their average for them. Easy !
+You just need to write a script.
+Return the average of the given array rounded down to its nearest integer.
+The array will never be empty.
+*/
+
+let getAverage = marks => Math.floor(marks.reduce((sum, x) => (sum + x))/ marks.length);
