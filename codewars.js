9586a571e5d5e455d8ab1adf7a65df0ab8d74af9
Eugene Kozlovsky <ekozlovskii@gmail.com>
2023-05-19 20:27:26 +0300

    add solution CodeWars "Abbreviate a Two Word Name"

--- codewars.js
+++ codewars.js
@@ -282,3 +282,16 @@ The array will never be empty.
 */
 
 let getAverage = marks => Math.floor(marks.reduce((sum, x) => (sum + x))/ marks.length);
+
+/* Abbreviate a Two Word Name
+Write a function to convert a name into initials. This kata strictly takes two words with one space
+in between them.
+The output should be two capital letters with a dot separating them.
+It should look like this:
+Sam Harris => S.H
+patrick feeney => P.F
+*/
+
+let abbrevName = name => name.split(" ").reduce((x, y) => x[0].toUpperCase()+ "." + y[0].toUpperCase());
+
+alert(abbrevName("Sam Harris"))
