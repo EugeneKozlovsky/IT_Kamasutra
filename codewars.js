5ea64be92793c7529393303980712693fab495b9
Eugene Kozlovsky <ekozlovskii@gmail.com>
2023-05-25 00:25:03 +0300

    add solution  "Remove First and Last Character"

--- codewars.js
+++ codewars.js
@@ -411,3 +411,21 @@ Examples (input -> output:
 */
 
 const flip = (d, a) => a.sort((x, y) => d === 'R' ? x - y : y - x);
+
+/* Remove First and Last Character Part Two
+You are given a string containing a sequence of character sequences separated by commas.
+Write a function which returns a new string containing the same character sequences except the first
+and the last ones but this time separated by spaces.
+If the input string is empty or the removal of the first and last items would cause the resulting string
+to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+Examples
+"1,2,3"      =>  "2"
+"1,2,3,4"    =>  "2 3"
+"1,2,3,4,5"  =>  "2 3 4"
+""     =>  NULL
+"1"    =>  NULL
+"1,2"  =>  NULL
+*/
+
+let array = string => string.split(",").slice(1,-1).join(" ") || null
