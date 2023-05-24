df4a80c2112ac8ae7f54167ef3e41e7ac28abd83
Eugene Kozlovsky <ekozlovskii@gmail.com>
2023-05-25 00:19:53 +0300

    add solution CodeWars "Removing Elements"

--- codewars.js
+++ codewars.js
@@ -382,3 +382,13 @@ Examples:
 */
 
 let isDivisible = (n, x, y) => n % x == 0 && n % y == 0;
+
+/* Removing Elements
+Take an array and remove every second element from the array. Always keep the first element and start
+removing with the next element.
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+None of the arrays will be empty, so you don't have to worry about that!
+*/
+
+let removeEveryOther = arr => arr.filter((_, i) => !(i % 2));
