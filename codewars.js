b377aea3b099f9d40ebee7d9fffb41d756b9f79f
Eugene Kozlovsky <ekozlovskii@gmail.com>
2023-05-24 22:13:42 +0300

    add solution CodeWars "Reversed Words"

--- codewars.js
+++ codewars.js
@@ -360,3 +360,13 @@ of 2 with the exponent ranging from 0 to n ( inclusive ).
 */
 
 let powersOfTwo = n => [...Array(n + 1)].map((_, i) => Math.pow(2, i));
+
+/* Reversed Words
+Complete the solution so that it reverses all of the words within the string passed in.
+Words are separated by exactly one space and there are no leading or trailing spaces.
+Example(Input --> Output):
+"The greatest victory is that which requires no battle" -->
+"battle no requires which that is victory greatest The"
+*/
+
+let reverseWords = str => str.split(" ").reverse().join(" ");
